eb4d2dae2f5228ed9902169dd0d3ea5129020c8b
Riccardo Trocca <rtrocca@gmail.com>
2021-04-02 17:49:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(name='wp_mini_api',
     version='0.1',
     description='A mini API to communicate with WordPress',
-    pacakges=find_packages(),
+    packages=find_packages(),
     install_requires=[
         'slugify'
     ]
